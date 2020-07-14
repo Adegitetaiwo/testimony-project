7f37f47a189c0b7f0979721f86b889fc4d6a6ff4
Adegite Taiwo <adegitetaiwo24@gmail.com>
2020-07-14 10:06:32 +0100

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn testimony.wsgi —-log-file -
+web: python testimony/manage.py collectstatic --noinput; gunicorn --pythonpath testimony testimony.wsgi
--- testimony/settings.py
+++ testimony/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG_PRODUCTION
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['apostolictestimony.herokuapp.com']
 
