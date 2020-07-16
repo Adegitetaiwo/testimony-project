0d12444a36ff7018802c9e493f9b280ede40a18e
Adegite Taiwo <adegitetaiwo24@gmail.com>
2020-07-16 01:54:31 +0100

    make it better

--- testimony/settings.py
+++ testimony/settings.py
@@ -320,10 +320,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiless')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 #add for MEDIA Files
 MEDIA_URL = '/media/'
 
