9356d52310f62629b359a376f3c6847dc60e3df6
Adegite Taiwo <adegitetaiwo24@gmail.com>
2020-07-16 01:49:34 +0100

    make it better

--- testimony/settings.py
+++ testimony/settings.py
@@ -320,7 +320,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiless')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
