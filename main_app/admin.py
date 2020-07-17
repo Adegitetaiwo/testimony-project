66be2c8f42a0cf480a1375167fb34aada2240b77
Adegite Taiwo <adegitetaiwo24@gmail.com>
2020-07-17 04:12:20 +0100

    header name

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,8 +4,9 @@ from .models import newTestimonies, askedQuestion, bibleQuote
 # Register your models here.
 class newTestimoniesAdmin(admin.ModelAdmin):
     list_display = ['title', 'approved', 'created', 'updated']
+    
 
-
+admin.site.site_header = 'Apostolic Testimony'
 admin.site.register(newTestimonies, newTestimoniesAdmin)
 admin.site.register(askedQuestion)
 admin.site.register(bibleQuote)
