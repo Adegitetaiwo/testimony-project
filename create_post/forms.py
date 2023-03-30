db6c48d752e36fc56c5c7e16046f86188caced14
Adegite Taiwo <adegitetaiwo24@gmail.com>
2023-03-30 03:26:21 +0100

    improved systems

--- create_post/forms.py
+++ create_post/forms.py
@@ -18,7 +18,8 @@ class createPostForms(forms.ModelForm):
     title = forms.CharField(max_length=260, required=True, widget=(forms.TextInput(attrs={'id':"fname", 'class':"form-control", 'placeholder':"Title : something to describe your Testimony"})))
     category = forms.ChoiceField(choices=CHOICES, required=False)
     #to be continue
-    body = forms.CharField(widget=CKEditorWidget(attrs={'cols': '80', 'rows': '30'}))
+    body = forms.CharField(required=False, widget=CKEditorWidget(attrs={'id': 'ckeditor', 'cols': "80", 'rows': "50", 'class': "form-control",
+                                                                 'placeholder': "Full narration of your testimony"}))
     author =forms.CharField(max_length= 200, required=False)
     author_img = forms.ImageField(required=False)
     email = forms.CharField(max_length=300, required=False, widget=(forms.TextInput(attrs={'type':"email", 'id':"email", 'class':"form-control", 'placeholder':"an email to track your testimony status"})))
